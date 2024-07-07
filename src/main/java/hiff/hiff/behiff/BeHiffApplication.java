aaa765048cbe0900ab8b02943c5c7b36a104da10
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-07-07 09:01:00 +0900

    [INIT]

---
+++ src/main/java/hiff/hiff/behiff/BeHiffApplication.java
@@ -0,0 +1,13 @@
+package hiff.hiff.behiff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeHiffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeHiffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hiff/hiff/behiff/BeHiffApplicationTests.java
@@ -0,0 +1,13 @@
+package hiff.hiff.behiff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeHiffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
