801b8b01b619ff0ef7c7af945db5d4b45cc1af9f
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-07-07 10:25:13 +0900

    [FEAT] user 탈퇴

---
+++ src/main/java/hiff/hiff/behiff/domain/user/presentation/controller/UserController.java
@@ -0,0 +1,32 @@
+package hiff.hiff.behiff.domain.user.presentation.controller;
+
+import hiff.hiff.behiff.domain.user.application.UserService;
+import hiff.hiff.behiff.domain.user.domain.entity.User;
+import hiff.hiff.behiff.global.auth.jwt.service.JwtService;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final JwtService jwtService;
+
+    @DeleteMapping
+    public ResponseEntity<Void> withdraw(HttpServletRequest request,
+        @AuthenticationPrincipal User user) {
+        Optional<String> accessToken = jwtService.extractAccessToken(request);
+        Optional<String> refreshToken = jwtService.extractRefreshToken(request);
+        userService.withdraw(user.getId(), accessToken, refreshToken);
+        return ResponseEntity.noContent().build();
+    }
+}
