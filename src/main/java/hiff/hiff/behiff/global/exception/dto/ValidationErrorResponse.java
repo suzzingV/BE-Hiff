0aafb4dc1beb78817c3b58a044fd6a8b2475b767
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-07-07 10:22:46 +0900

    [CHORE] 예외 설정

---
+++ src/main/java/hiff/hiff/behiff/global/exception/dto/ValidationErrorResponse.java
@@ -0,0 +1,13 @@
+package hiff.hiff.behiff.global.exception.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ValidationErrorResponse {
+
+    private String field;
+    private String message;
+    private Object rejectedValue;
+}
---
+++ src/main/java/hiff/hiff/behiff/global/exception/properties/ErrorCode.java
@@ -0,0 +1,38 @@
+package hiff.hiff.behiff.global.exception.properties;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    // 400
+    REFRESH_TOKEN_REQUIRED(BAD_REQUEST, "refresh token이 필요합니다."),
+
+    // 401
+    SECURITY_UNAUTHORIZED(UNAUTHORIZED, "인증 정보가 유효하지 않습니다"),
+    SECURITY_INVALID_TOKEN(UNAUTHORIZED, "토큰이 유효하지 않습니다."),
+    SECURITY_INVALID_REFRESH_TOKEN(UNAUTHORIZED, "refresh token이 유효하지 않습니다."),
+    SECURITY_INVALID_ACCESS_TOKEN(UNAUTHORIZED, "access token이 유효하지 않습니다."),
+
+    // 403
+    SECURITY_ACCESS_DENIED(FORBIDDEN, "접근 권한이 없습니다."),
+
+    // 404
+    USER_NOT_FOUND(NOT_FOUND, "user을 찾을 수 없습니다."),
+
+    // 500
+    SERVER_ERROR(INTERNAL_SERVER_ERROR, "예상치 못한 서버 에러가 발생하였습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
