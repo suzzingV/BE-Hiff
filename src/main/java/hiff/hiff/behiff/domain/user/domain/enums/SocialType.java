b3f33a756bff79a2af573b8e799ebfe53d528da0
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-07-17 16:52:20 +0900

    [REFACT] 유효성 추가

--- src/main/java/hiff/hiff/behiff/domain/user/domain/enums/SocialType.java
+++ src/main/java/hiff/hiff/behiff/domain/user/domain/enums/SocialType.java
@@ -1,5 +1,10 @@
 package hiff.hiff.behiff.domain.user.domain.enums;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public enum SocialType {
-    GOOGLE, KAKAO, NAVER
+    GOOGLE, KAKAO, NAVER;
+
+    public static final Set<SocialType> ALL_VALUES = EnumSet.allOf(SocialType.class);
 }
