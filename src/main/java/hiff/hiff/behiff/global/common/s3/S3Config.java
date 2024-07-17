0106a83e7410edc4b5fbbdb5bafe28fed4fcc63a
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-07-17 16:53:08 +0900

    [CHORE] 사진 등록 임시 무효화

--- src/main/java/hiff/hiff/behiff/global/common/s3/S3Config.java
+++ src/main/java/hiff/hiff/behiff/global/common/s3/S3Config.java
@@ -10,29 +10,29 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class S3Config {
-
-    @Value("${aws.bucket.end-point}")
-    private String endPoint;
-
-    @Value("${aws.bucket.region-name}")
-    private String regionName;
-
-    @Value("${aws.bucket.access-key")
-    private String accessKey;
-
-    @Value("${aws.bucket.secret-key")
-    private String secretKey;
-
-    @Value("${aws.bucket.name}")
-    private String bucketName;
-
-    @Bean
-    public AmazonS3 amazonS3() {
-        return AmazonS3ClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
-                .build();
-    }
-}
+//@Configuration
+//public class S3Config {
+//
+////    @Value("${aws.bucket.end-point}")
+//    private String endPoint;
+//
+////    @Value("${aws.bucket.region-name}")
+//    private String regionName;
+//
+////    @Value("${aws.bucket.access-key")
+//    private String accessKey;
+//
+////    @Value("${aws.bucket.secret-key")
+//    private String secretKey;
+//
+////    @Value("${aws.bucket.name}")
+//    private String bucketName;
+//
+//    @Bean
+//    public AmazonS3 amazonS3() {
+//        return AmazonS3ClientBuilder.standard()
+//                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
+//                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+//                .build();
+//    }
+//}
--- src/main/java/hiff/hiff/behiff/global/common/s3/S3Service.java
+++ src/main/java/hiff/hiff/behiff/global/common/s3/S3Service.java
@@ -18,39 +18,39 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Component
-@RequiredArgsConstructor
-@Transactional
-public class S3Service {
-
-    private final AmazonS3 s3;
-
-    @Value("${aws.bucket.end-point}")
-    private String endPoint;
-
-    @Value("${aws.bucket.name}")
-    private String bucketName;
-
-    public List<String> savePhotos(List<MultipartFile> photos) {
-        List<String> photoUrls = new ArrayList<>();
-        for (MultipartFile photo : photos) {
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentLength(photo.getSize());
-            objectMetadata.setContentType(photo.getContentType());
-            String photoName = UUID.randomUUID().toString(); // 고유한 파일 이름 부여
-            try {
-                PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, photoName, photo.getInputStream(), objectMetadata)
-                        .withCannedAcl(CannedAccessControlList.PublicRead); // url로 접근할 수 있도록 파일 공개 설정
-                s3.putObject(putObjectRequest);
-            } catch (SdkClientException | IOException e) {
-                throw new S3Exception(ErrorCode.S3_ACCESS_DENIED, e.getMessage());
-            }
-
-            photoUrls.add(endPoint + "/" + photoName);
-        }
-
-        return photoUrls;
-    }
-
-
-}
+//@Component
+//@RequiredArgsConstructor
+//@Transactional
+//public class S3Service {
+//
+//    private final AmazonS3 s3;
+//
+//    @Value("${aws.bucket.end-point}")
+//    private String endPoint;
+//
+//    @Value("${aws.bucket.name}")
+//    private String bucketName;
+//
+//    public List<String> savePhotos(List<MultipartFile> photos) {
+//        List<String> photoUrls = new ArrayList<>();
+//        for (MultipartFile photo : photos) {
+//            ObjectMetadata objectMetadata = new ObjectMetadata();
+//            objectMetadata.setContentLength(photo.getSize());
+//            objectMetadata.setContentType(photo.getContentType());
+//            String photoName = UUID.randomUUID().toString(); // 고유한 파일 이름 부여
+//            try {
+//                PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, photoName, photo.getInputStream(), objectMetadata)
+//                        .withCannedAcl(CannedAccessControlList.PublicRead); // url로 접근할 수 있도록 파일 공개 설정
+//                s3.putObject(putObjectRequest);
+//            } catch (SdkClientException | IOException e) {
+//                throw new S3Exception(ErrorCode.S3_ACCESS_DENIED, e.getMessage());
+//            }
+//
+//            photoUrls.add(endPoint + "/" + photoName);
+//        }
+//
+//        return photoUrls;
+//    }
+//
+//
+//}
