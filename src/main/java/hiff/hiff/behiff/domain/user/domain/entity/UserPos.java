baf12689f8207d8f0f0407e0261b607b0c12e7af
suzzingv <sjlim1999@naver.com>
2024-08-02 00:09:38 +0900

    [FEAT] user 현재 위치 추가

---
+++ src/main/java/hiff/hiff/behiff/domain/user/domain/entity/UserPos.java
@@ -0,0 +1,45 @@
+package hiff.hiff.behiff.domain.user.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.joda.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user_pos_TB")
+@Entity
+public class UserPos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String x;
+
+    @Column(nullable = false, length = 20)
+    private String y;
+
+    private Long userId;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    private UserPos(String x, String y, Long userId) {
+        this.x = x;
+        this.y = y;
+        this.userId = userId;
+    }
+
+    public void changePos(String x, String y, LocalDateTime updatedAt) {
+        this.x = x;
+        this.y = y;
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ src/main/java/hiff/hiff/behiff/domain/user/infrastructure/UserPosRepository.java
@@ -0,0 +1,11 @@
+package hiff.hiff.behiff.domain.user.infrastructure;
+
+import hiff.hiff.behiff.domain.user.domain.entity.Belief;
+import hiff.hiff.behiff.domain.user.domain.entity.UserPos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserPosRepository extends JpaRepository<UserPos, Long> {
+    Optional<UserPos> findByUserId(Long userId);
+}
