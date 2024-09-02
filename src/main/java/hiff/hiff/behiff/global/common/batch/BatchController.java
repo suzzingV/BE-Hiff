a7835075e6cb74a7df3ddf417de322e4647ab43c
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-09-03 01:57:11 +0900

    [TEST] 배치 테스트 컨트롤러

--- src/main/java/hiff/hiff/behiff/global/common/batch/BatchController.java
+++ src/main/java/hiff/hiff/behiff/global/common/batch/BatchController.java
@@ -1,16 +1,11 @@
 package hiff.hiff.behiff.global.common.batch;
 
-import hiff.hiff.behiff.domain.matching.application.service.MatchingService;
-import hiff.hiff.behiff.domain.user.domain.entity.User;
-import hiff.hiff.behiff.domain.user.domain.entity.UserHobby;
-import hiff.hiff.behiff.domain.user.domain.entity.UserLifeStyle;
 import hiff.hiff.behiff.domain.user.infrastructure.UserHobbyRepository;
 import hiff.hiff.behiff.domain.user.infrastructure.UserLifeStyleRepository;
 import hiff.hiff.behiff.domain.user.infrastructure.UserRepository;
 import hiff.hiff.behiff.global.common.redis.RedisService;
-import java.time.Duration;
+import hiff.hiff.behiff.global.common.scheduler.Scheduler;
 import java.util.Date;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
@@ -28,49 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/batch")
+@RequiredArgsConstructor
 public class BatchController {
 
-    private final Job getUserByGenderJob;
-    private final JobLauncher jobLauncher;
-    private final UserRepository userRepository;
-    private final UserHobbyRepository userHobbyRepository;
-    private final UserLifeStyleRepository userLifeStyleRepository;
-    private final RedisService redisService;
-
-    public BatchController(@Qualifier("getUserByGenderJob") Job getUserByGenderJob, JobLauncher jobLauncher, UserHobbyRepository userHobbyRepository, UserRepository userRepository, RedisService redisService, UserLifeStyleRepository userLifeStyleRepository) {
-        this.getUserByGenderJob = getUserByGenderJob;
-        this.jobLauncher = jobLauncher;
-        this.userRepository = userRepository;
-        this.userHobbyRepository = userHobbyRepository;
-        this.redisService = redisService;
-        this.userLifeStyleRepository = userLifeStyleRepository;
-    }
+    private final Scheduler scheduler;
 
     @GetMapping("/gender")
     public ResponseEntity<Void> getUserByGender()
         throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
-//        List<User> all = userRepository.findAll();
-//        for(User female : all) {
-//            List<UserHobby> userHobbies = userHobbyRepository.findByUserId(female.getId());
-//            List<UserLifeStyle> userLifeStyles = userLifeStyleRepository.findByUserId(female.getId());
-//
-//            StringBuilder tmp = new StringBuilder();
-//            for (UserHobby userHobby : userHobbies) {
-//                tmp.append(userHobby.getHobbyId()).append("/");
-//            }
-//            redisService.setStrValue("ushob_" + female.getId(), tmp.toString(), Duration.ofDays(2));
-//
-//            tmp = new StringBuilder();
-//            for (UserLifeStyle userLifeStyle : userLifeStyles) {
-//                tmp.append(userLifeStyle.getLifeStyleId()).append("/");
-//            }
-//            redisService.setStrValue("uslif_" + female.getId(), tmp.toString(), Duration.ofDays(2));
-//        }
-
-        JobParameters jobParameters = new JobParametersBuilder()
-            .addDate("currentTime", new Date())
-            .toJobParameters();
-        jobLauncher.run(getUserByGenderJob, jobParameters);
+        scheduler.freeHiffMatching();
         return ResponseEntity.ok().build();
 
     }
