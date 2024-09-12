682538d311998a0b28efa6e4862d1d95a6b095ef
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-09-13 08:32:35 +0900

    [FIX] fcmkeyfile 경로 수정

--- src/main/java/hiff/hiff/behiff/global/common/fcm/FcmConfig.java
+++ src/main/java/hiff/hiff/behiff/global/common/fcm/FcmConfig.java
@@ -5,26 +5,31 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import hiff.hiff.behiff.global.auth.exception.AuthException;
 import hiff.hiff.behiff.global.response.properties.ErrorCode;
+import java.io.InputStream;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import org.springframework.util.ResourceUtils;
 
 @Configuration
 @Slf4j
 public class FcmConfig {
 
+    @Value("${fcm.credentials.location}")
+    private String keyFileName;
+
     @PostConstruct
     private void init() {
-        String fileResourceURL = "fcm-keyfile.json";
-        ClassPathResource resource = new ClassPathResource(fileResourceURL);
 
         FirebaseOptions options = null;
         try {
+            InputStream keyFile = ResourceUtils.getURL(keyFileName).openStream();
             options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
+                    .setCredentials(GoogleCredentials.fromStream(keyFile))
                     .build();
         } catch (IOException e) {
             throw new AuthException(ErrorCode.FCM_INIT_ERROR, e.getMessage());
