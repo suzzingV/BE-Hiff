97dcf6181c3129e55f04583f554935c8c0ff37a9
suzzingv <sjlim1999@naver.com>
2024-09-20 00:24:50 +0900

    [HOTFIX] 유저 가중치 조회 희망 거리나이 추가

--- src/main/java/hiff/hiff/behiff/domain/user/presentation/dto/res/UserWeightValueResponse.java
+++ src/main/java/hiff/hiff/behiff/domain/user/presentation/dto/res/UserWeightValueResponse.java
@@ -9,13 +9,21 @@ public class UserWeightValueResponse {
     private Integer hobbyWeight;
     private Integer lifeStyleWeight;
     private Integer mbtiWeight;
-    public static UserWeightValueResponse of(Long userId, Integer appearanceWeight, Integer hobbyWeight, Integer lifeStyleWeight, Integer mbtiWeight) {
+    private Integer hopeMinAge;
+    private Integer hopeMaxAge;
+    private Integer minDistance;
+    private Integer maxDistance;
+    public static UserWeightValueResponse of(Long userId, Integer appearanceWeight, Integer hobbyWeight, Integer lifeStyleWeight, Integer mbtiWeight, Integer hopeMinAge, Integer hopeMaxAge, Integer minDistance, Integer maxDistance) {
         return UserWeightValueResponse.builder()
                 .userId(userId)
                 .appearanceWeight(appearanceWeight)
                 .hobbyWeight(hobbyWeight)
                 .lifeStyleWeight(lifeStyleWeight)
                 .mbtiWeight(mbtiWeight)
+                .hopeMinAge(hopeMinAge)
+                .hopeMaxAge(hopeMaxAge)
+                .maxDistance(maxDistance)
+                .minDistance(minDistance)
                 .build();
     }
 }
